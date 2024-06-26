45a971f1f7b291794788b13986c8fd43ff534304
Karmadeepsinh Jadeja <karmadeepsinhjadeja444@gmail.com>
2024-06-26 16:07:14 +0530

    removed logout button

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
             {/* <Navbar.Brand href="#home" >Hello {user}</Navbar.Brand> */}
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
-              <Button variant="primary">Logout</Button>
+              {/* <Button variant="primary">Logout</Button> */}
             </Navbar.Collapse>
           </Container>
         </Navbar>
